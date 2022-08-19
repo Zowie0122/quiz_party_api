b68659622a72fc251adf98ac717f745c3ad9266b
Zowie0122 <minjiayi22@gmail.com>
2022-08-19 16:35:50 +0900

    [+] added random quiz dammy data

--- db/dammyData.js
+++ db/dammyData.js
@@ -1,13 +1,14 @@
-const quizs = [{
-  question: "How many legs does the Legs of Man have?",
-  options: [1, 2, 3, 4],
-  bingo: 2
-},
-{
-  question: "Which sea creature has three hearts?",
-  options: ["Rabbit", "Bear", "Pig", "Octopus"],
-  bingo: 3
-}
-]
+const quizs = [
+  {
+    question: 'How many legs does the Legs of Man have?',
+    options: ['One', 'Two', 'Three', 'Four'],
+    bingo: 2,
+  },
+  {
+    question: 'Which sea creature has three hearts?',
+    options: ['Rabbit', 'Bear', 'Pig', 'Octopus'],
+    bingo: 3,
+  },
+];
 
-module.exports = quizs
+module.exports = quizs;
